f272f321e93a14099da05d6be355a07804c77db1
Jason Hundal <jasonhundal23@gmail.com>
2019-10-06 19:12:23 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -3,7 +3,7 @@ def bonus
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "dead"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
@@ -19,6 +19,7 @@ def bonus
       ]
    }
   }
-puts epic_tragedy[:montague][:hero][status]
+epic_tragedy[:montague][:hero][status] = "dead"
+epic_tragedy[:montague][:heroine][status] = "dead"
   epic_tragedy
 end
