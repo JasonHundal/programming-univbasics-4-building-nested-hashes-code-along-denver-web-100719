cbe7469724461893eb65100bb4d365583f842cc2
Jason Hundal <jasonhundal23@gmail.com>
2019-10-06 19:15:43 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,6 +19,7 @@ def bonus
       ]
    }
   }
-  puts epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:montague][:heroine][:status] = "dead"
   epic_tragedy
 end
